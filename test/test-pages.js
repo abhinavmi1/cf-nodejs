caf4e58bbe798abb6c50e8fc4bc6bd2cda17bb3d
Abhinav Mishra <abhinavmi1@users.noreply.github.com>
2018-11-18 21:31:59 -0800

    Update test-pages.js

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,9 +1,31 @@
-var expect  = require('chai').expect;
-var request = require('request');
 
-it('Main page content', function(done) {
-    request('http://localhost:9080' , function(error, response, body) {
-        expect(body).to.equal('Hello World');
-        done();
+var assert = require('chai').assert;
+
+describe('team module', function() {
+    var team;
+    
+    beforeEach(function() {
+        team = require('../app/team')({
+            User: {
+                find: function(team, cb) {
+                    cb(null, [ 'Superman', 'Batman', 'Flash' ]);
+                }
+            }
+        });
+    });
+    
+    it('should return members from mock', function( done ) {
+        
+        team.findMembers( 'justice-league', function(err, data) {
+            
+            if (err) { return done( err ); }
+            
+            assert.equal( data.length, 3 );
+            assert.deepEqual( data, ['Superman', 'Batman', 'Flash']  );
+            
+            done();
+            
+        } );
+        
     });
 });
